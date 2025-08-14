dc34882113796490598afc9d1996c33860634d96
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-14 16:04:35 -0300

    feat: add component Picture

---
+++ src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="avatar" :alt="descricao"> <!-- " : " é igual a v-bind -->
+</template>
+
+<script>
+export default {
+  name: "Picture",
+  data() {
+    return {
+      avatar: "/img/avatar.png",
+      descricao: "foto do avatar"
+    } 
+  }
+}
+</script>
