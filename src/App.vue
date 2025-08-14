96abd750002911c8d8e3e1846d9cd1e5ce9b5632
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-14 13:01:51 -0300

    feat: add main.js and App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <LifeCycle />
+    <Pessoa/>
+  </div>
+</template>
+
+<script>
+import LifeCycle from './components/LifeCycle.vue'
+import Pessoa from './components/Pessoa.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LifeCycle,
+    Pessoa,
+  }
+}
+</script>
