c7a55fb61a38a9c2e4cd9a026534f28f42b5d3ae
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-18 11:11:24 -0300

    feat: add header

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Projetos</a></li>
+    <li><a href="#">Contatos</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Header"
+
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  display: flex;
+  background: #444444;
+}
+
+li{
+  margin-right: 10px;
+}
+
+a {
+  color: #ddd;
+  text-decoration: none;
+  transition: .5s;
+}
+
+a:hover {
+  color: rgb(255, 193, 23);
+}
+</style>
