295b2da26d0fb007629b01653fd5b8d7a2975a7b
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-18 11:15:16 -0300

    feat: add global scop local scop <style> - App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <Header/>
     <LifeCycle />
+    <p class="teste-pai">teste component pai</p>
     <Pessoa/>
   </div>
 </template>
@@ -8,12 +10,29 @@
 <script>
 import LifeCycle from './components/LifeCycle.vue'
 import Pessoa from './components/Pessoa.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
     LifeCycle,
     Pessoa,
   }
 }
 </script>
+
+<style>
+body {
+  background-color: #333;
+  color: #fff
+}
+
+a {
+  color: red;
+}
+
+.teste {
+  background-color: #000;
+}
+</style>
