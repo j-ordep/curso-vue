cef32b99adc738ae36079d7c57d6ada336ee668c
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-18 11:13:01 -0300

    feat: add v-for - Info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,17 +2,21 @@
   <div>
     <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
     <p v-else>Estou em busca de uma oportunidade de emprego</p>
-    <p>Utilizo as seguintes tecnologias:</p>
+    <p>Utilizo as seguintes tecnologias para backend:</p>
     <ul>
-      <li>Go</li>
-      <li>Java</li>
-      <li>JavaScript</li>
+      <li v-for="(technology, index) in backend_technologies" v-bind:key="index">{{ technology }}</li>
+    </ul>
+    <p>Utilizo as seguintes tecnologias para frontend:</p>
+    <ul>
+      <li v-for="technology in frontend_technologies">
+        {{ technology.id}} - {{ technology.language }}
+      </li>
     </ul>
     <div>
-      <button @click="showEmail">Mostrar email</button>
+      <button @click="showEmail">{{ bnt_text }}</button>
     </div>
     <p v-show="mostrar_email">E-mail: {{ email }}</p>
-    <p>para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">clique aqui</a></p>
+    <p class="teste">para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">clique aqui</a></p>
   </div>
 
   <Picture/>
@@ -29,16 +33,35 @@ export default {
   data() {
     return {
       esta_trabalhando: true,
-      mostrar_email: true,
+      mostrar_email: false,
       email: "email@gmail.com",
       meu_link: "http://google.com",
+      bnt_text: "mostrar email",
+      backend_technologies: ["Go", "Java", "JavaScript"],
+      frontend_technologies: [
+        {id: 1, language: "HTML"},
+        {id: 2, language: "CSS"},
+        {id: 3, language: "Vue"},
+      ]
     }
   },
   methods: {
     showEmail() {
-        this.mostrar_email = !this.mostrar_email
+      this.mostrar_email = !this.mostrar_email
+      if (this.mostrar_email == false) {
+          this.bnt_text = "mostrar email"
+      } else {
+          this.bnt_text = "esconder email"
+        }
     }
   }, 
   
 }
 </script>
+
+<!-- não recomendado pois gera bagunça -->
+<style> 
+.teste-pai {
+  color: aqua;
+}
+</style>
